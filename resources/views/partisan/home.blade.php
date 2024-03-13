c2da7c4f6068b2c4ea4eda681631c60654265651
BlueCode <82035540+WellkitDev@users.noreply.github.com>
2024-03-13 22:45:40 +0700

    Initial commit

---
+++ resources/views/partisan/home.blade.php
@@ -0,0 +1,36 @@
+<!--====================================
+=            Hero Section            =
+=====================================-->
+<section class="section gradient-banner" id="home">
+	<div class="shapes-container">
+		<div class="shape" data-aos="fade-down-left" data-aos-duration="1500" data-aos-delay="100"></div>
+		<div class="shape" data-aos="fade-down" data-aos-duration="1000" data-aos-delay="100"></div>
+		<div class="shape" data-aos="fade-up-right" data-aos-duration="1000" data-aos-delay="200"></div>
+		<div class="shape" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200"></div>
+		<div class="shape" data-aos="fade-down-left" data-aos-duration="1000" data-aos-delay="100"></div>
+		<div class="shape" data-aos="fade-down-left" data-aos-duration="1000" data-aos-delay="100"></div>
+		<div class="shape" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="300"></div>
+		<div class="shape" data-aos="fade-down-right" data-aos-duration="500" data-aos-delay="200"></div>
+		<div class="shape" data-aos="fade-down-right" data-aos-duration="500" data-aos-delay="100"></div>
+		<div class="shape" data-aos="zoom-out" data-aos-duration="2000" data-aos-delay="500"></div>
+		<div class="shape" data-aos="fade-up-right" data-aos-duration="500" data-aos-delay="200"></div>
+		<div class="shape" data-aos="fade-down-left" data-aos-duration="500" data-aos-delay="100"></div>
+		<div class="shape" data-aos="fade-up" data-aos-duration="500" data-aos-delay="0"></div>
+		<div class="shape" data-aos="fade-down" data-aos-duration="500" data-aos-delay="0"></div>
+		<div class="shape" data-aos="fade-up-right" data-aos-duration="500" data-aos-delay="100"></div>
+		<div class="shape" data-aos="fade-down-left" data-aos-duration="500" data-aos-delay="0"></div>
+	</div>
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-md-6 order-2 order-md-1 text-center text-md-left">
+				<h1 class="text-white title font-weight-bold mb-4">ANAGATA SEMBAGI<br><span>EDUCATION.</span></h1>
+				<p class="text-white mb-5">Nawala Education: Research and Publishing adalah lembaga publikasi jurnal terpercaya dan professional.</p>
+				<a href="404.html" class="btn btn-main-md">PUBLISHING NOW</a>
+			</div>
+			<div class="col-md-6 text-center order-1 order-md-2">
+				<img class="img-fluid" src="images/bg-title.svg" alt="screenshot">
+			</div>
+		</div>
+	</div>
+</section>
+<!--====  End of Hero Section  ====-->
---
+++ resources/views/partisan/services.blade.php
@@ -0,0 +1,25 @@
+<section class="section pt-0 position-relative pull-top" id="service">
+	<div class="container">
+		<div class="rounded shadow p-5 bg-white">
+			<h3 class="text-center">Kami menyediakan layanan publikasi jurnal dengan kualitas yang baik! <br>dengan tahapan sebagai berikut:</h3>
+			<div class="row mt-4">
+				<div class="col-lg-4 col-md-6 mt-5 mt-md-0 text-center">
+					<i class="ti-comments text-primary h1"></i>
+					<h3 class="mt-4 text-capitalize h5 ">Konsultasi</h3>
+					<p class="regular text-muted">Konsultasikan dengan staff kami apa yang Anda cari dan apa kemauan Anda!</p>
+				</div>
+				<div class="col-lg-4 col-md-6 mt-5 mt-md-0 text-center">
+					<i class="ti-cloud-up text-primary h1"></i>
+					<h3 class="mt-4 text-capitalize h5 ">Proses Publikasi</h3>
+					<p class="regular text-muted">Kami akan memproses publikasi jurnal Anda sesuai estimasi waktu yang akan diberikan!</p>
+				</div>
+				<div class="col-lg-4 col-md-12 mt-5 mt-lg-0 text-center">
+					<i class="ti-rss-alt text-primary h1"></i>
+					<h3 class="mt-4 text-capitalize h5 ">Publikasi Selesai</h3>
+					<p class="regular text-muted">Jurnal Anda telah usai di proses dan sekarang resmi terpublikasi!</p>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
